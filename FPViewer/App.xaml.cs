78d6df68f48843bb111a2dc2e488d1efbc3fb420
castella53 <castella53@outlook.com>
2013-05-30 07:57:23 +0900

    Console output Unhandled Exceptionn

--- FPViewer/App.xaml.cs
+++ FPViewer/App.xaml.cs
@@ -13,5 +13,15 @@ namespace FPViewer
     /// </summary>
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            string mes = string.Format(
+                "Unhandled Error:\n\n---\n\n{0}\n\n{1}",
+                e.Exception.Message, e.Exception.StackTrace);
+
+            Console.WriteLine(mes);
+            // 例外を処理したことを通知
+            e.Handled = true;
+        }
     }
 }
